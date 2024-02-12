c998268e7e0d2904585e3063bb98580f026325b6
yuankongzhe <yuankongzhe@gmail.com>
2024-02-12 18:48:47 +0800

    修复app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,24 @@
 import "@/styles/globals.css";
 import Head from 'next/head';
+import Script from 'next/script'; // 引入Script组件
+
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-      <link 
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"
-      />
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        <link 
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" 
+          rel="stylesheet" 
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" 
+          crossOrigin="anonymous"
+        />
       </Head>
+      <Script 
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" 
+        crossOrigin="anonymous"
+        strategy="beforeInteractive"
+      />
       <Component {...pageProps} />
     </>
   );
