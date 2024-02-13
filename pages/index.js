178c85ce1039558543dede63ac3c4218ef1029f7
yuankongzhe <yuankongzhe@gmail.com>
2024-02-13 08:00:40 +0800

    Automated commit message

--- pages/index.js
+++ pages/index.js
@@ -202,7 +202,8 @@ const HomePage = () => {
             <svg width="24" height="24" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1458"><path d="M928 254.3c-30.6 13.2-63.9 22.7-98.2 26.4 35.4-21.1 62.3-54.4 75-94-32.7 19.5-69.7 33.8-108.2 41.2C765.4 194.6 721.1 174 672 174c-94.5 0-170.5 76.6-170.5 170.6 0 13.2 1.6 26.4 4.2 39.1-141.5-7.4-267.7-75-351.6-178.5-14.8 25.4-23.2 54.4-23.2 86.1 0 59.2 30.1 111.4 76 142.1-28-1.1-54.4-9-77.1-21.7v2.1c0 82.9 58.6 151.6 136.7 167.4-14.3 3.7-29.6 5.8-44.9 5.8-11.1 0-21.6-1.1-32.2-2.6C211 652 273.9 701.1 348.8 702.7c-58.6 45.9-132 72.9-211.7 72.9-14.3 0-27.5-0.5-41.2-2.1C171.5 822 261.2 850 357.8 850 671.4 850 843 590.2 843 364.7c0-7.4 0-14.8-0.5-22.2 33.2-24.3 62.3-54.4 85.5-88.2z" p-id="1459" fill="#000"></path></svg>
             @0xfaskety</a>  创建
             </div>
-            <span class="h3">Merlin Seal TVL: {formatNumber(c.toFixed(0))} USD</span>
+            <span className="h3">Merlin Seal TVL: {formatNumber(c.toFixed(0))} USD</span>
+            <p className="h5 text-success">过去24小时新增约: {formatNumber(sumdata.data.changein24.toFixed(0))} USD</p>
           </div>
           <div className='card-body container ' >
             <div className='row '>
