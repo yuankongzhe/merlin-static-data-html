8d0750b2288a40ef78d37c7332fe493bed97e34d
yuankongzhe <yuankongzhe@gmail.com>
2024-03-20 21:06:29 +0800

    Automated commit message

--- pages/index.js
+++ pages/index.js
@@ -1106,9 +1106,9 @@ const HomePage = () => {
                       <div className='row text-end'>
                         <img src="/pre.png" className='border border-primary rounded shadow-lg p-3 mb-5 bg-body rounded' alt="TVL预测曲线" style={{ maxWidth: '100%', height: 'auto' }}/>
                         <span className='text-muted'>由于BTC价格上涨以及ETHS的质押开启，相较原公式引入了修正项<br></br>
-                        截至03-08，目前场外MERL平均成交价为<br></br>
-                        3.3 USDT（来自<a href="https://app.whales.market/" target="_blank" rel="noopener noreferrer">whales</a>平台）<br></br>
-                        2.9 USDT（来自企鹅OTC群: 383301198）
+                        截至03-20，目前场外MERL平均成交价为<br></br>
+                        2.7 USDT（来自<a href="https://app.whales.market/" target="_blank" rel="noopener noreferrer">whales</a>平台）<br></br>
+                        2.7 USDT（来自企鹅OTC群: 383301198）
                         </span>
                       </div>
                       <div className='row'>                     
